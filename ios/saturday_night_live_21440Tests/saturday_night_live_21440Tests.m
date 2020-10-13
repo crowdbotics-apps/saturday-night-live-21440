a4b5974b78f9878def80d9d1766cf5a253d1ed17
Crowdbotics <team@crowdbotics.com>
2020-10-13 17:32:33 +0000

    Initial commit.

---
+++ ios/saturday_night_live_21440Tests/saturday_night_live_21440Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface saturday_night_live_21440 : XCTestCase
+
+@end
+
+@implementation saturday_night_live_21440
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
